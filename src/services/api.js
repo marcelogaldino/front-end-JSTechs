f314e921f763920d017c580efe11e584959a19e6
marcelogaldino <marcelinhomgo@gmail.com>
2020-03-22 11:02:02 -0300

    configuring variables evironment

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://node-api-jshuntt.herokuapp.com/api/' })
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL })
 
 export default api
